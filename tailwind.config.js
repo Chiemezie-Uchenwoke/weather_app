9dcac67f199435e75fd86ada0c6070d182270c79
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2024-12-22 17:40:08 +0100

    Add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "League Spartan": ["Poppins", "serif"]
+      },
+      colors: {
+        "pri-blue": "#133E87",
+        "sec-blue": "#608BC1",
+        "acc-blue": "#CBDCEB",
+        "sec-gold": "#ffca2a"
+      }
+    },
   },
   plugins: [],
 }
