ac7d92472ae6d7811013c59541772680e1dae5a0
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-01-14 00:00:01 +0100

    complete

--- public/js/weatherbg.js
+++
@@ -1,5 +0,0 @@
-/* const bgImageElement = document.body;
-const image = "./public/images/weather-bg.jpg";
-bgImageElement.style.backgroundImage = `url('${image}')`;
-bgImageElement.style.backgroundSize = "cover";
-bgImageElement.style.backgroundRepeat = "no-repeat"; */
